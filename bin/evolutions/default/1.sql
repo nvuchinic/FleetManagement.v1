ee9804a434a49d550a5943703f3ee8b1d127b542
nvuchinic <nermin.vucinic@gmail.com>
2015-07-30 14:23:07 +0200

    mergedWithLogin

--- bin/evolutions/default/1.sql
+++ bin/evolutions/default/1.sql
@@ -70,9 +70,22 @@ create table truck (
   model                     varchar(255),
   year                      varchar(255),
   num_of_containers         integer,
+  status                    varchar(255),
   constraint pk_truck primary key (id))
 ;
 
+<<<<<<< HEAD
+create table vendor (
+  id                        integer not null,
+  name                      varchar(255),
+  address                   varchar(255),
+  city                      varchar(255),
+  country                   varchar(255),
+  phone                     varchar(255),
+  email                     varchar(255),
+  constraint pk_vendor primary key (id))
+;
+=======
 create sequence admin_seq;
 
 create sequence employee_seq;
@@ -80,10 +93,13 @@ create sequence employee_seq;
 create sequence manager_seq;
 
 create sequence reset_password_seq;
+>>>>>>> 970d256092e4d5af5c3895cc6590c37d99a54226
 
 create sequence train_seq;
 
 create sequence truck_seq;
+
+create sequence vendor_seq;
 
 
 
@@ -104,6 +120,8 @@ drop table if exists train;
 
 drop table if exists truck;
 
+drop table if exists vendor;
+
 SET REFERENTIAL_INTEGRITY TRUE;
 
 drop sequence if exists admin_seq;
@@ -117,4 +135,6 @@ drop sequence if exists reset_password_seq;
 drop sequence if exists train_seq;
 
 drop sequence if exists truck_seq;
+
+drop sequence if exists vendor_seq;
 
